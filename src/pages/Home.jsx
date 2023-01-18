b2f0c2e108f81609eda29dbf8f34ac27612e19d8
whitemagic <shivambohracse@gmail.com>
2023-01-18 23:20:25 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/Users/UserResults";
+
+function Home() {
+  return (
+    <>
+      {/* search component */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
